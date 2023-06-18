53622f29e0b9c7df6e8c34df243d0cf91737fcb5
tong <2959547540@qq.com>
2023-06-19 03:36:38 +0800

    1.增加了index>index.html，连接了相关static中css/js。index中内容不全，需要图片更换及内容补充。2.使用时注意修改settings数据库中的user及password。3.另提醒models中大部分数据库需要重写

--- index/views.py
+++ index/views.py
@@ -11,35 +11,35 @@ def user(request):
 
 def news(request):
     return render(request, 'news.html')
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            next_url = request.GET.get('next', '/')
-            return redirect(next_url)
-        else:
-            # 登录失败，返回错误信息给模板
-            error_message = 'Invalid username or password.'
-            return render(request, 'login.html', {'error_message': error_message})
-    else:
-        return render(request, 'login.html')
-
-def register(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        # 创建新用户
-        user = User.objects.create_user(username=username, email=email, password=password)
-        # 登录新用户
-        login(user)
-
-        # 注册成功，重定向到某个页面
-        return redirect('login.html')
-    else:
-        return render(request, 'register.html')
+#
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             next_url = request.GET.get('next', '/')
+#             return redirect(next_url)
+#         else:
+#             # 登录失败，返回错误信息给模板
+#             error_message = 'Invalid username or password.'
+#             return render(request, 'login.html', {'error_message': error_message})
+#     else:
+#         return render(request, 'login.html')
+#
+# def register(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         # 创建新用户
+#         user = User.objects.create_user(username=username, email=email, password=password)
+#         # 登录新用户
+#         login(user)
+#
+#         # 注册成功，重定向到某个页面
+#         return redirect('login.html')
+#     else:
+#         return render(request, 'register.html')
 
